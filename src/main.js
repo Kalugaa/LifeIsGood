81e119263401b5802af008e9710bcad0dc42ac5d
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-07-06 21:53:35 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/mobile-menu';
+import './js/sections/hero';
+import './js/sections/how-it-works';
+import './js/sections/vegetables';
+import './js/sections/we-will-call-you';
+import './js/footer';
+import './js/modal';
