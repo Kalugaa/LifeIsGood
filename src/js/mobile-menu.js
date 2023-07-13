2dd5c0d9a6902d5298c01908b19a5fef7d53c6ae
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-07-13 13:37:49 +0200

    update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,17 @@ let menuBtn = document.querySelector('.mobile-burger-icon');
 let menu = document.querySelector('.menu');
 let links = document.querySelectorAll('.mobile-list a');
 
-menuBtn.addEventListener('click', function(){
+// Функція для скидання стану кнопки меню до початкового стану
+function resetMenuButton() {
+  menuBtn.classList.remove('active');
+  menuBtn.innerHTML = `
+    <svg class="mobile-burger-icon" id="icon" width="24" height="24">
+      <use href="/img/symbol-defs.svg#mobile-burger-icon"></use>
+    </svg>`;
+  document.body.style.overflow = ''; // Відновлення прокрутки
+}
+
+menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
 
@@ -12,25 +22,15 @@ menuBtn.addEventListener('click', function(){
         <path d="M18 6L6 18" stroke="#77AC63" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M6 6L18 18" stroke="#77AC63" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>`;
-    document.body.style.overflow = 'hidden'; // Блокировка прокрутки
+    document.body.style.overflow = 'hidden'; // Блокування прокрутки
   } else {
-    menuBtn.innerHTML = `
-      <svg class="mobile-burger-icon" id="icon" width="24" height="24">
-        <use href="/img/symbol-defs.svg#mobile-burger-icon"></use>
-      </svg>`;
-    document.body.style.overflow = ''; // Восстановление прокрутки
+    resetMenuButton();
   }
 });
 
-// Add event listeners to each link
-links.forEach(function(link) {
-  link.addEventListener('click', function() {
-    menuBtn.classList.remove('active');
-    menu.classList.remove('active');
-    menuBtn.innerHTML = `
-      <svg class="mobile-burger-icon" id="icon" width="24" height="24">
-        <use href="/img/symbol-defs.svg#mobile-burger-icon"></use>
-      </svg>`;
-    document.body.style.overflow = ''; // Восстановление прокрутки
+// Додаємо обробники подій до кожного посилання
+links.forEach(function (link) {
+  link.addEventListener('click', function () {
+    resetMenuButton();
   });
 });
